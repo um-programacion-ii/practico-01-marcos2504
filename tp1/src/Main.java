7810a5652538811b27936f2bdba709a00129913e
marcos2504 <marcosibarra1234@gmail.com>
2024-03-19 19:36:52 -0300

    1

---
+++ tp1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Crear ingredientes
+        Ingrediente huevo = new Ingrediente("Huevo", 10);
+        Ingrediente agua = new Ingrediente("Agua", 1000);
+
+
+        System.out.println(huevo);
+        System.out.println(agua);
+
+    }
+}
