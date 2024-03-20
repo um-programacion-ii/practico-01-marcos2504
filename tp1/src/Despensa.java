60444580ea2fe47c205e4b0219102543d765e69e
marcos2504 <marcosibarra1234@gmail.com>
2024-03-20 18:17:29 -0300

    item2 terminado

--- tp1/src/Despensa.java
+++ tp1/src/Despensa.java
@@ -1,3 +1,59 @@
 public class Despensa {
-    pr
+    private Ingrediente[] ingredientes;
+    private int contador;
+
+    public Despensa() {
+        this.ingredientes = new Ingrediente[10]; // Tamaño de la despensa
+        this.contador = 0;
+    }
+
+    public void addIngrediente(Ingrediente ingrediente) {
+        if (contador < ingredientes.length) {
+            boolean ingredienteEncontrado = false;
+
+            // Buscar el ingrediente en la despensa
+            for (int i = 0; i < contador; i++) {
+                if (ingredientes[i].getNombre().equals(ingrediente.getNombre())) {
+                    // Si el ingrediente ya está en la despensa, actualizar la cantidad
+                    ingredientes[i].setCantidad(ingredientes[i].getCantidad() + ingrediente.getCantidad());
+                    ingredienteEncontrado = true;
+                    break;
+                }
+            }
+
+            // Si el ingrediente no se encuentra en la despensa, agregarlo
+            if (!ingredienteEncontrado) {
+                // Agregar el ingrediente a la despensa
+                ingredientes[contador] = ingrediente;
+                contador++;
+            }
+        } else {
+            System.out.println("Despensa llena, no se puede agregar más ingredientes.");
+        }
+    }
+
+
+    public void getIngrediente(String nombreIngrediente, int cantidad) {
+        for (int i = 0; i < contador; i++) {
+            if (ingredientes[i].getNombre().equals(nombreIngrediente)) {
+                Ingrediente ingredienteEnDespensa = ingredientes[i];
+                if (ingredienteEnDespensa.getCantidad() >= cantidad) {
+                    // Reducir la cantidad del ingrediente en la despensa
+                    ingredienteEnDespensa.sacar(cantidad);
+                    return; // No es necesario devolver nada, simplemente salimos del método
+                } else {
+                    System.out.println("No hay suficiente cantidad de " + nombreIngrediente + " en la despensa.");
+                    return; // Salir del método si no hay suficiente cantidad
+                }
+            }
+        }
+        System.out.println("El ingrediente " + nombreIngrediente + " no está presente en la despensa.");
+    }
+    public void mostrarDespensa() {
+        System.out.println("Contenido de la despensa:");
+        for (int i = 0; i < contador; i++) {
+            Ingrediente ingrediente = ingredientes[i];
+            System.out.println("Nombre: " + ingrediente.getNombre() + ", Cantidad: " + ingrediente.getCantidad());
+        }
+    }
 }
--- tp1/src/Main.java
+++ tp1/src/Main.java
@@ -7,6 +7,22 @@ public class Main {
 
         System.out.println(huevo);
         System.out.println(agua);
+        Despensa despensa = new Despensa();
+
+        // Agregar ingredientes a la despensa
+        despensa.addIngrediente(huevo);
+        despensa.addIngrediente(agua);
+        despensa.addIngrediente(new Ingrediente("Harina", 750));
+
+        // Mostrar el contenido de la despensa
+        despensa.mostrarDespensa();
+        despensa.getIngrediente( "Huevo" ,  10);
+        despensa.mostrarDespensa();
+        despensa.addIngrediente(new Ingrediente("Harina", 750));
+        despensa.mostrarDespensa();
+        
+
+
 
     }
 }
